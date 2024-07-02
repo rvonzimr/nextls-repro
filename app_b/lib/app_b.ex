85390ceae0fb237f223af93ddb97898df63d695f
Robert Von Zimmerman <robert.zimmerman@humanly.io>
2024-07-02 13:42:58 -0700

    init

---
+++ app_b/lib/app_b.ex
@@ -0,0 +1,19 @@
+defmodule AppB do
+  @moduledoc """
+  Documentation for `AppB`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> AppB.hello()
+      :world
+
+  """
+  
+  def hello do
+    IO.inspect("Hi");
+  end
+end
